92792a3307e4fba21d24d6d130f9196cc8187951
simon <SimonJi@SimonMacPro.local>
2017-08-08 16:49:35 +0800

    Finished 2 assignments

---
+++ LAFF-2.0xM/Programming/Week01/laff_copy_simon.m
@@ -0,0 +1,15 @@
+function [ y_out ] = laff_copy_simon( x, y )
+[m_x, n_x] = size(x);
+[m_y, n_y] = size(y);
+
+if(m_x ~= 1 && n_x ~= 1 || m_y ~= 1 && n_y ~= 1 || m_x * n_x ~= m_y * n_y)
+    y_out = 'FAILED';
+    return
+end
+
+y_out = zeros(m_y, n_y);
+
+for i = 1:m_x * n_x
+    y_out(i) = x(i);
+end
+end
